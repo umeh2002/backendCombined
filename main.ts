010797ee7a98104b213e011e500030afcba7c398
umeh emmanuel <eumeh3882@gmail.com>
2023-08-08 08:23:46 -0700

    default get

--- main.ts
+++ main.ts
@@ -1,11 +1,23 @@
 import cors from "cors"
-import express, { Application } from "express"
+import express, { Application, Request,Response } from "express"
 import auth from "./Router/authRouter"
 import task from "./Router/taskRouter"
 
 const main =(app:Application)=>{
     app.use(express.json())
     app.use(cors())
+
+    app.get("/", (req: Request, res: Response) => {
+        try {
+          res.status(200).json({
+            message: "Welcome Page!!!",
+          });
+        } catch (error) {
+          res.status(404).json({
+            message: "error",
+          });
+        }
+      });
 
     app.use("/api/v1/auth" , auth)
     app.use("/api/v1/task" , task)
