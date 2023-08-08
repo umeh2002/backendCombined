47e054212562e7604ef1c5c9ee3241897446fc1c
umeh emmanuel <eumeh3882@gmail.com>
2023-08-08 08:26:37 -0700

    done default get

--- src/dist/main.js
+++ src/dist/main.js
@@ -10,6 +10,18 @@ const taskRouter_1 = __importDefault(require("./Router/taskRouter"));
 const main = (app) => {
     app.use(express_1.default.json());
     app.use((0, cors_1.default)());
+    app.get("/", (req, res) => {
+        try {
+            res.status(200).json({
+                message: "Welcome Page!!!",
+            });
+        }
+        catch (error) {
+            res.status(404).json({
+                message: "error",
+            });
+        }
+    });
     app.use("/api/v1/auth", authRouter_1.default);
     app.use("/api/v1/task", taskRouter_1.default);
 };
